f1e8ed2213a7cad4e29243cc948774df40f4b790
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-02-21 01:22:57 +0000

    Refactor Vector2 class and remove unused code in Program.cs

--- OpenUI-Test/Program.cs
+++ OpenUI-Test/Program.cs
@@ -4,7 +4,6 @@ using OpenUI.Mathematics;
 public static class Program {
   public static void Main(string[] args) {
     var pos = new Vector2(0, 0);
-    pos.X = sizeof(int);
     Console.WriteLine(pos);
   }
 }
--- OpenUI/Mathematics.cs
+++ OpenUI/Mathematics.cs
@@ -1,12 +1,19 @@
+using System.Runtime.CompilerServices;
+
 namespace OpenUI.Mathematics {
-  public class Vector2 {
+  public unsafe class Vector2 {
     public float X;
     public float Y;
-    
+
     public Vector2(float x, float y) {
       X = x;
       Y = y;
     }
+
+    /// <summary>
+    /// Defines the size of the Vector2 struct in bytes.
+    /// </summary>
+    public static readonly int SizeInBytes = sizeof(Vector2);
 
     public override string ToString() {
       return $"({X}, {Y})";
