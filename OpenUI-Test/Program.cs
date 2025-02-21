63fbbf7496b49ed3c6f9e819edd133a33bd5b81f
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-02-20 17:17:18 -0700

    Update Program.cs

--- OpenUI-Test/Program.cs
+++ OpenUI-Test/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using OpenUI.Mathematics;
+
+public static class Program {
+  public static void Main(string[] args) {
+    var pos = new Vector2(10 * sizeof(int), 5 * sizeof(int));
+    Console.WriteLine(pos);
+  }
+}
