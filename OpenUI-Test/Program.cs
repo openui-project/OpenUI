011d2efac992900deed8cdf2deaa82d632443e5b
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-02-20 17:36:30 -0700

    Update Program.cs

--- OpenUI-Test/Program.cs
+++ OpenUI-Test/Program.cs
@@ -3,7 +3,7 @@ using OpenUI.Mathematics;
 
 public static class Program {
   public static void Main(string[] args) {
-    var pos = Vector2.Zero;
+    var pos = new Vector2(0, 0);
     pos.X = sizeof(int);
     Console.WriteLine(pos);
   }
