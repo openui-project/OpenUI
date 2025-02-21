c01e3c9c3190f2313b3319e661c4e9d522c75261
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-02-20 17:14:44 -0700

    Update Mathematics.cs

--- OpenUI/Mathematics.cs
+++ OpenUI/Mathematics.cs
@@ -1,2 +1,15 @@
 namespace OpenUI.Mathematics {
+  public class Vector2 {
+    public readonly float X;
+    public readonly float Y;
+    
+    public Vector2(float x, float y) {
+      X = x;
+      Y = y;
+    }
+    
+    public override string ToString() {
+      return $"({X}, {Y})";
+    }
+  }
 }
