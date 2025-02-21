65ba3f5d78674b7d88ef4a332f011b025c242b99
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-02-20 22:29:34 -0500

    Update Mathematics.cs

--- OpenUI/Mathematics.cs
+++ OpenUI/Mathematics.cs
@@ -2,21 +2,17 @@ using System;
 
 namespace OpenUI.Mathematics {
   public unsafe class Vector2 {
-    public float X;
-    public float Y;
+    public float X, Y;
 
     public Vector2(float x, float y) {
       X = x;
       Y = y;
     }
 
-    /// <summary>
-    /// Defines the size of the Vector2 class in bytes.
-    /// </summary>
+    public readonly float Length => (float)Math.Sqrt(X * X + Y * Y);
+
     public static readonly int SizeInBytes = sizeof(Vector2);
 
-    public override string ToString() {
-      return $"({X}, {Y})";
-    }
+    public override string ToString() => $"({X}, {Y})";
   }
 }
