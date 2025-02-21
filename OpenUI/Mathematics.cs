e467b52af1f23423f3f9d923068b28672d5c11ac
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-02-20 17:29:20 -0700

    Update Mathematics.cs

--- OpenUI/Mathematics.cs
+++ OpenUI/Mathematics.cs
@@ -1,13 +1,8 @@
 namespace OpenUI.Mathematics {
   public class Vector2 {
-    public readonly float X;
-    public readonly float Y;
+    public float X;
+    public float Y;
     
-    public Vector2(float value) {
-      X = value;
-      Y = value;
-    }
-
     public Vector2(float x, float y) {
       X = x;
       Y = y;
