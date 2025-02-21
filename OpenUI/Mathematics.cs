acc85bb6a13979fa09791bb1e17b4e158da733e1
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-02-20 17:32:38 -0700

    Update Mathematics.cs

--- OpenUI/Mathematics.cs
+++ OpenUI/Mathematics.cs
@@ -8,6 +8,8 @@ namespace OpenUI.Mathematics {
       Y = y;
     }
 
+    public static readonly Vector2 Zero = new Vector2(0, 0);
+    
     public override string ToString() {
       return $"({X}, {Y})";
     }
